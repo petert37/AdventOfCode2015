ec6162495b619cb376eb65b0e8cd6706a0a51e76
Peter Toth <dev.toth.peter37@gmail.com>
2015-12-10 18:35:15 +0100

    Major optimization

--- Day10/src/hu/petert/aoc/d10/Main.java
+++ Day10/src/hu/petert/aoc/d10/Main.java
@@ -5,29 +5,23 @@ public class Main {
     public static final int STEPS = 50;
 
     public static void main(String[] args){
-
-        String input = "3113322113";
-
+        StringBuffer input = new StringBuffer("3113322113");
         for (int i = 0; i < STEPS; i++) {
-            char[] chars = input.toCharArray();
-            String newInput = "";
+            StringBuffer newInput = new StringBuffer();
             int count = 1;
-            char c = chars[0];
-            for (int j = 1; j < chars.length; j++) {
-                if(chars[j] == c) count++;
+            char c = input.charAt(0);
+            for (int j = 1; j < input.length(); j++) {
+                if(input.charAt(j) == c) count++;
                 else {
-                    newInput = newInput.concat(String.valueOf(count) + c);
+                    newInput.append(count).append(c);
                     count = 1;
-                    c = chars[j];
+                    c = input.charAt(j);
                 }
             }
-            newInput = newInput.concat(String.valueOf(count) + c);
+            newInput.append(count).append(c);
             input = newInput;
-            System.out.println(i);
         }
-
-        System.out.println("Result length " + input.length());
-
+        System.out.println("Result length: " + input.length());
     }
 
 }
